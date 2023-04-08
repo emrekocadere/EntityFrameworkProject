60d5f176a4175f0102637da276f1570ad122913b
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-04-09 00:50:22 +0300

    Add Coach Entity

---
+++ Entites/Coach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    internal class Coach
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int LeagueId { get; set; }
+        public League League { get; set; }
+
+
+    }
+}
