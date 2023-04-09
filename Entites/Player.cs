603c7f8261fc26464d7e50236c69cfa4ae365e6e
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-04-09 20:43:38 +0300

    Add Player Entity

---
+++ Entites/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    internal class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+
+
+        
+    }
+}
