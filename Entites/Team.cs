d0da2f7301b93014e6a23a9dcbe06c9bf2c98967
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-04-09 21:03:03 +0300

    Modify Team Entity(Add "Players" navigation property)

--- Entites/Team.cs
+++ Entites/Team.cs
@@ -9,6 +9,6 @@ namespace Entites
     internal class Team
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public List<Player> Players { get; set; }
     }
 }
