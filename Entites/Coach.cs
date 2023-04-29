b2567ff851dfcba300aae7373fe4c201a29de122
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-04-30 01:12:48 +0300

    Modify  Coach Class(Override ToString() Function)

--- Entites/Coach.cs
+++ Entites/Coach.cs
@@ -8,6 +8,10 @@ namespace Entites
 {
     public class Coach 
     {
+        public override string ToString()
+        {
+            return "ID: "+Id+" , Name: "+Name+" , Surname: "+Surname+" Team: "+Team.Name;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
